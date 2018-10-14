e2cd687bd91a8e4928f1618ca78e4024a4999758
vijayanbu <vijay@ontroapp.com>
2018-10-14 15:48:37 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToDoComponent } from './to-do/to-do.component';
+import { SearchComponent } from './search/search.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent,
+    SearchComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/to-do/to-do.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do',
+  templateUrl: './to-do.component.html',
+  styleUrls: ['./to-do.component.css']
+})
+export class ToDoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  clickMe = (v) => {
+    alert(v)
+  }
+
+}
