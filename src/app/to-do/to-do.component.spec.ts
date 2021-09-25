33bd9fe6fa111f0f69b9a357a09daf8ebb909fb2
Vijay <vijayanbu001@gmail.com>
2021-09-25 19:51:33 +0530

    fix security vulnerabilities

--- src/app/to-do/to-do.component.spec.ts
+++ src/app/to-do/to-do.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ToDoComponent } from './to-do.component';
 
@@ -6,7 +6,7 @@ describe('ToDoComponent', () => {
   let component: ToDoComponent;
   let fixture: ComponentFixture<ToDoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ToDoComponent ]
     })
